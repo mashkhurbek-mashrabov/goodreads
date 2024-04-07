26882dbb45d55ec287beecaa239555e398c05eff
Mashkhurbek <mashkhurbek.mashrabov@gmail.com>
2024-04-07 12:08:40 +0500

    added Author, Book, BookReview models

--- app/apps/books/admin.py
+++ app/apps/books/admin.py
@@ -1,3 +1,25 @@
 from django.contrib import admin
 
-# Register your models here.
+from books.models import Author, Book, BookReview
+
+
+@admin.register(Author)
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ('name', 'email', 'created_at')
+    list_filter = ('created_at',)
+    search_fields = ('name', 'email')
+
+
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    list_display = ('title', 'isbn', 'created_at')
+    list_filter = ('authors', 'created_at')
+    search_fields = ('title', 'isbn', 'description')
+    filter_horizontal = ('authors',)
+
+
+@admin.register(BookReview)
+class BookReviewAdmin(admin.ModelAdmin):
+    list_display = ('book', 'stars')
+    list_filter = ('stars', 'created_at')
+    search_fields = ('comment', 'book__title')
--- app/apps/books/models.py
+++ app/apps/books/models.py
@@ -1,3 +1,37 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.core.validators import MinValueValidator, MaxValueValidator
 
-# Create your models here.
+from common.models import BaseModelV2
+
+
+class Author(BaseModelV2):
+    name = models.CharField(max_length=100)
+    email = models.EmailField()
+    bio = models.TextField()
+
+    def __str__(self):
+        return self.name
+
+
+class Book(BaseModelV2):
+    title = models.CharField(max_length=15)
+    description = models.TextField()
+    authors = models.ManyToManyField(Author, related_name="books")
+    isbn = models.CharField(max_length=17)
+
+    def __str__(self):
+        return self.title
+
+
+class BookReview(BaseModelV2):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="reviews")
+    book = models.ForeignKey(Book, on_delete=models.CASCADE, related_name="reviews")
+    comment = models.CharField(max_length=256)
+    stars = models.SmallIntegerField(validators=[
+        MinValueValidator(1), MaxValueValidator(5)
+    ])
+
+    def __str__(self):
+        return f"{self.book} | {self.user}"
+
